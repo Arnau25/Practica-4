e897b36d78b75a5a38c0b34c90dabc954f6c2515
Arnau25 <81559427+Arnau25@users.noreply.github.com>
2021-06-17 12:23:46 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,51 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void ledON (void * pvParameters);
+void ledOFF(void * pvParameters);
+int LED =2; 
+
+SemaphoreHandle_t semafor;
+
+void setup(){
+
+Serial.begin(9600);
+pinMode(LED, OUTPUT);
+semafor= xSemaphoreCreateMutex();
+
+xTaskCreate(
+    ledON,
+    "LED ON",
+    10000,
+    NULL,
+    1,
+    NULL);
+
+xTaskCreate(
+    ledOFF,
+    "LED OFF",
+    10000,
+    NULL,
+    1,
+    NULL);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){}
+
+void ledON (void * pvParameters){
+    for(;;){
+        xSemaphoreTake(semafor, portMAX_DELAY);
+        digitalWrite(LED, HIGH);
+        delay(3000);
+        xSemaphoreGive(semafor);
+    }
+}
+
+void ledOFF (void * pvParameters){
+    for(;;){
+        xSemaphoreTake(semafor, portMAX_DELAY);
+        digitalWrite(LED, LOW);
+        delay(1500);
+        xSemaphoreGive(semafor);
+    }
 }
